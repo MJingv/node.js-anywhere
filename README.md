bd909746deaef15f38cc29432de650832c2611d4
MJingv <625619954@qq.com>
2017-10-08 18:57:52 +0800

    cli

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
 # node.js-anywhere
+
 my NodeJs Static Web Server
 
 ## how to use ??
+
 continuing
 
 ## tips
+
 supervisor -- 自动监听文件修改并重启server
+
+## install
+
+```
+
+npm i -g anywhere
+```
+
+## way to use
+
+```
+
+anywhere  # 把当前文件夹作为静态资源服务器根目录
+
+anywhere -p 8080 # 把端口号设置为任意（eg：8080）
+
+anywhere -h localhost # 把主机设置为任意（eg：localhost）
+
+anywhere -d /user # 把根目录设置为任意（eg：/usr）
+
+```
--- src/app.js
+++ src/app.js
@@ -4,11 +4,21 @@ const path = require('path')
 const conf = require('./config/defaultConf')
 const router = require('./helper/router')
 
-const server = http.createServer((req, res) => {
-  const filePath = path.join(conf.root, req.url)
-  router(req, res, filePath)
-})
-server.listen(conf.port, conf.hostname, () => {
-  const addr = `http://${conf.hostname}:${conf.port}`
-  console.info(`server started at ${chalk.green(addr)}`)
-})
+class Server {
+  constructor(config) {
+    this.conf = Object.assign({}, conf, config)
+    //将默认conf和用户输入的config合并起来==>浅拷贝
+  }
+  start() {
+    const server = http.createServer((req, res) => {
+      const filePath = path.join(this.conf.root, req.url)
+      router(req, res, filePath, this.conf)
+    })
+    server.listen(this.conf.port, this.conf.hostname, () => {
+      const addr = `http://${this.conf.hostname}:${this.conf.port}`
+      console.info(`server started at ${chalk.green(addr)}`)
+    })
+  }
+}
+
+module.exports = Server
